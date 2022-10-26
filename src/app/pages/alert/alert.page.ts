c320111f6928db8be0307a59d16c40ea58700dd4
Tindat1 <tindat@asistencia.com.ar>
2022-10-26 11:55:00 -0300

    Alertas

--- src/app/pages/alert/alert.page.ts
+++ src/app/pages/alert/alert.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AlertController } from '@ionic/angular';
+import config from '../../../../capacitor.config';
 
 @Component({
   selector: 'app-alert',
@@ -15,6 +16,7 @@ export class AlertPage implements OnInit {
 
   async presentAlert() {
     const alert = await this.alertCtrl.create({
+      backdropDismiss : false,
       message:'This is an alert message',
       header: 'Alert!',
       buttons: ['OK']
@@ -23,4 +25,72 @@ export class AlertPage implements OnInit {
     await alert.present();
 
   }
+
+  async presentAlertMultipleButtons() {
+    const alert = await this.alertCtrl.create({
+      backdropDismiss : false,
+      message:'This is an alert message',
+      header: 'Alert!',
+      buttons: [
+        {
+          text : 'OK',
+          handler : () => {
+            console.log('Clickeo OK')
+          }
+        },
+        {
+          text : 'Cancel',
+          role : 'cancel',
+          cssClass: 'rojo',
+
+        },
+      ]
+      });
+
+    await alert.present();
+
+  }
+
+  async presentAlertprompt() {
+    const alert = await this.alertCtrl.create({
+      backdropDismiss : false,
+      header: 'Please enter your info',
+      buttons: [{
+        text : 'OK',
+        handler : (data:any) => {
+          console.log( data )
+        }
+      }],
+      animated: false,
+      inputs: [
+        {
+          placeholder: 'Name',
+          type: 'text',
+          name : 'Nombre',
+        },
+        {
+          placeholder: 'Nickname (max 8 characters)',
+          name : 'Sobrenombre',
+          attributes: {
+            maxlength: 8,
+          },
+        },
+        {
+          type: 'number',
+          placeholder: 'Age',
+          name : 'Edad',
+          min: 1,
+          max: 134,
+        },
+        {
+          type: 'textarea',
+          name : 'TextArea',
+          placeholder: 'A little about yourself',
+        },
+      ],
+    });
+
+    await alert.present();
+  }
 }
+
